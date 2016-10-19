1e6979e4818d2b5ef65193eab0065fc53747521e
dayson108 <davidaking87@gmail.com>
2016-10-19 18:15:21 +0000

    ext file fix

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+app.use('/', express.static(__dirname + '/'));
 var io = require('socket.io').listen(server);
 server.listen(process.env.PORT || 3000);
+
+console.log("listening");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
 io.on('connection', function(socket){
   console.log('a user connected');
 
