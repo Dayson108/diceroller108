660a241fc4644c18f7e973358fca869f7e7499ba
davidaking87 <david.king@ucdenver.edu>
2017-03-17 19:32:16 -0600

    2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -6,19 +7,19 @@ app.use('/', express.static(__dirname + '/'));
 var io = require('socket.io').listen(server);
 app.use(bodyParser.urlencoded({extended: true}));
 var mongodb = require('mongodb');
-
-var Players = [];
-
-var PlayerList = [];
-var PlayerIndex = 0;
-
-
-var InitList = [];
-var DMData = {
-	CName: "",
-	PName: "",
-	ID: ""
-};
+*/
+var express = require('express'),   
+http = require('http'),
+app = express(),
+server = http.createServer(app),
+io = require('socket.io').listen(server),
+bcrypt = require('bcryptjs');
+app.use('/', express.static(__dirname + '/'));
+var mongodb = require('mongodb');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 
 //drd108's database
@@ -39,139 +40,6 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || 'mongodb://dayson108:5tar
 	});
 });
 
-app.get('/test', function(req, res){
-  res.sendFile(__dirname + '/html/Comment.html');
-});
 app.get('/', function(req, res){
-		console.log("Dirname: " + __dirname);
   res.sendFile(__dirname + '/index.html');
 });
-
-function findPlayersIndex(socket){
-		for(var i = 0; i < Players.length; i++){
-			if(Players[i].playerID == socket){
-				return i;
-			}
-		}		
-}
-
-
-io.on('connection', function(socket){
-	io.to(socket.id).emit('SendStartingData', socket.id, DMData, PlayerList, InitList);
-	//Recieved by everyone EXCEPT sender 
-	//socket.broadcast.emit('', msg);
-	
-	//Recieved by everyone
-	//io.sockets.emit('', msg);
-	
-	//io.to(socketid).emit('message', 'for your eyes only');
-		
-	socket.on('disconnect', function(){
-		if(socket.id != DMData.ID){
-			var rmvIndex = findPlayersIndex(socket.id);
-			Players.splice(rmvIndex, 1);
-			io.to(DMData.ID).emit('RcvDMData', Players);
-		}
-		
-		
-		//Removes from the player list*******
-		if(socket.id == DMData.ID){
-			DMData.CName = "";
-			DMData.PName = "";
-			DMData.ID = "";
-			io.sockets.emit('ClearDM', DMData); 
-		}
-		else{
-			var i = 0;
-			var index = -1;
-			for (i = 0; i < PlayerList.length; i++){
-				if(PlayerList[i].playerID == socket.id){
-					index = i;
-				}
-			}
-			if(index >= 0){
-				PlayerList.splice(index, 1);
-			}
-		}
-		io.sockets.emit('UpdatePlayerList', PlayerList);			
-		//***********************************
-		
-
-	});
-	
-	socket.on('UpdatePlayer', function(iChar){
-		var index = findPlayersIndex(socket.id);
-		Players[index] = iChar;
-		io.to(DMData.ID).emit('RcvDMData', Players);
-	});
-		
-		
-	socket.on('sidedDiceRoll', function(msg){
-		socket.broadcast.emit('sidedDiceResult', msg);
-	});
-	 
-	socket.on('20DiceRoll', function(msg){
-		socket.broadcast.emit('20DiceResult', msg);
-	});
-	
-	socket.on('DMSubmitted', function(msg){
-		DMData.CName = msg.CName;
-		DMData.PName = msg.PName;
-		DMData.ID = socket.id;
-		io.sockets.emit('UpdateDM', DMData, PlayerList);
-		io.to(DMData.ID).emit('RcvDMData', Players);
-	});
-	
-	socket.on('AddPlayer', function(msg){
-		var temp = msg;
-		Players.push(msg);
-		if(DMData.ID){
-			io.to(DMData.ID).emit('UpdatePlayerStats', Players);
-			io.to(DMData.ID).emit('RcvDMData', Players);
-		}	
-	});
-
-
-	socket.on('playerSubmitted', function(msg){
-		var newPlayer = {
-			charName: msg.CName,
-			playerName: msg.PName,
-			playerID: socket.id
-		};
-	
-		PlayerList.push(newPlayer);
-		io.sockets.emit('UpdatePlayerList', PlayerList);
-		
-	});
-	
-	socket.on('Test', function(){
-		//console.log("");
-		//console.log('Testing...');
-	});
-	
-	socket.on('SendPvtMsg', function(msgToId, msg){
-		io.to(msgToId).emit('PvtMsgRcv', msg);
-	});
-	
-
-	
-	
-	socket.on('InitRoll', function(input){
-		input.playerID = socket.id;
-		InitList.push(input);
-		io.sockets.emit('ServerUpdateInitList', InitList);
-	});
-
-	socket.on('ClearInit', function(){
-		InitList = [];
-		io.sockets.emit('ClearInitRecieved');
-	});	
-	
-	socket.on('ChatMsgSend', function(msg){
-		io.sockets.emit('ChatMsgRcv', msg);
-	});
-
-
-
-});
-
